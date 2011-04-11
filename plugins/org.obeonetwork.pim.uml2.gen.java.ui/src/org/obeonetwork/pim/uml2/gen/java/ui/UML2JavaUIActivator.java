59db6cc913cd22397a5279f334bc3bda6176b926
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2011-04-11 15:13:53 +0200

    First version of the UI project.

---
+++ plugins/org.obeonetwork.pim.uml2.gen.java.ui/src/org/obeonetwork/pim/uml2/gen/java/ui/UML2JavaUIActivator.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephane Begaudeau (Obeo) - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.pim.uml2.gen.java.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ * @since 0.1
+ */
+public class UML2JavaUIActivator extends AbstractUIPlugin {
+
+	/**
+	 * The plug-in ID.
+	 */
+	public static final String PLUGIN_ID = "org.obeonetwork.pim.uml2.gen.java.ui";
+
+	/**
+	 * The shared instance.
+	 */
+	private static UML2JavaUIActivator plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public UML2JavaUIActivator() {
+	}
+
+	/**{@inheritDoc}
+	 *
+	 * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+	 * @generated
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/**{@inheritDoc}
+	 *
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 * @generated
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance.
+	 *
+	 * @return the shared instance
+	 */
+	public static UML2JavaUIActivator getDefault() {
+		return plugin;
+	}
+
+}
---
+++ plugins/org.obeonetwork.pim.uml2.gen.java.ui/src/org/obeonetwork/pim/uml2/gen/java/ui/common/GenerateAll.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephane Begaudeau (Obeo) - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.pim.uml2.gen.java.ui.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.obeonetwork.pim.uml2.gen.java.main.Workflow;
+
+
+/**
+ * Main entry point of the 'UML2 to Java' generation module.
+ * @since 0.1
+ */
+public class GenerateAll {
+
+	/**
+	 * The model URI.
+	 */
+	private URI modelURI;
+
+	/**
+	 * The output folder.
+	 */
+	private File targetFolder;
+
+	/**
+	 * The other arguments.
+	 */
+	List<? extends Object> arguments;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param modelURI
+	 *            is the URI of the model.
+	 * @param targetFolder
+	 *            is the output folder
+	 * @param arguments
+	 *            are the other arguments
+	 * @throws IOException
+	 *             Thrown when the output cannot be saved.
+	 * @generated
+	 */
+	public GenerateAll(URI modelURI, File targetFolder, List<? extends Object> arguments) {
+		this.modelURI = modelURI;
+		this.targetFolder = targetFolder;
+		this.arguments = arguments;
+	}
+
+	/**
+	 * Launches the generation.
+	 *
+	 * @param monitor
+	 *            This will be used to display progress information to the user.
+	 * @throws IOException
+	 *             Thrown when the output cannot be saved.
+	 * @generated
+	 */
+	public void doGenerate(IProgressMonitor monitor) throws IOException {
+		if (!targetFolder.exists()) {
+			targetFolder.mkdirs();
+		}
+		
+		Workflow generator = new Workflow(modelURI, targetFolder, arguments);
+		generator.doGenerate(BasicMonitor.toMonitor(monitor));
+		
+	}
+}
