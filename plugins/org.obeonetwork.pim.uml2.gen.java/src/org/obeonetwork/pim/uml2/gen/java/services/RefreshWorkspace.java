1ec2138ed9da981e65c89a5dbd330cda379d85e2
mcartaud <mathieu.cartaud@obeo.fr>
2012-12-11 17:04:23 +0100

    add the choice to generate OSGi bundle or not

--- plugins/org.obeonetwork.pim.uml2.gen.java/src/org/obeonetwork/pim/uml2/gen/java/services/RefreshWorkspace.java
+++ plugins/org.obeonetwork.pim.uml2.gen.java/src/org/obeonetwork/pim/uml2/gen/java/services/RefreshWorkspace.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephane Begaudeau (Obeo) - initial API and implementation
+ *     Mathieu Cartaud (Obeo) - OSGi implementation
+ *******************************************************************************/
 package org.obeonetwork.pim.uml2.gen.java.services;
 
 import org.eclipse.core.resources.IProject;
